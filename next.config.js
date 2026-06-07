dbead337f316bc6d4ef5d2dea5680251fbee0606
Bhaskar Anand <official.beingbhaskar@gmail.com>
2026-06-07 06:52:55 +0530

    Configure Turbopack configuration to resolve Next.js 16 build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  turbopack: {},
 
   // Reduce build time by skipping type checking (run separately)
   typescript: {
